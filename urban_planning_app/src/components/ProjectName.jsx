9f89fb1984704d5a65ee933910513077ddfa6d58
Chester Wonchoba <cjwchoba@gmail.com>
2024-04-06 13:39:48 -0500

    Update ProjectName.jsx

--- urban_planning_app/src/components/ProjectName.jsx
+++ urban_planning_app/src/components/ProjectName.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Button from 'react-bootstrap/Button';
 
 function ProjectName() {
     return (
-        <h1>Urban Planning Simulation</h1>
-
+        <h1>Urban Planning</h1>
+        
     )
 }
 
