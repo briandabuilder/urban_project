755d0c3921e45c78454f5121db9be7a8d4be5e56
Chester Wonchoba <cjwchoba@gmail.com>
2024-04-06 13:28:07 -0500

    Update ProjectName.jsx

---
+++ urban_planning_app/src/components/ProjectName.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+
+function ProjectName() {
+    return (
+        <h1></h1>
+
+    )
+}
+
+export default ProjectName;
